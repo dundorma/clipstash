badbb9d0d232fac6711e381e5bc08a9353bad933
dundorma <90293691+dundorma@users.noreply.github.com>
2023-08-21 22:52:27 +0700

    feat: complete domain's time.rs

---
+++ src/lib/domain/time.rs
@@ -0,0 +1,35 @@
+use chrono::{DateTime, NaiveDateTime, Utc};
+use derive_more::From;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[derive(Debug, Clone, From, Deserialize, Serialize)]
+pub struct Time(DateTime<Utc>);
+
+impl Time {
+    // pub fn new() -> Self {
+    //     Self(DateTime::now())
+    // }
+
+    pub fn into_inner(self) -> DateTime<Utc> {
+        self.0
+    }
+
+    pub fn timestamp(&self) -> i64 {
+        self.0.timestamp()
+    }
+
+    pub fn from_naive_utc(datetime: NaiveDateTime) -> Self {
+        Time(DateTime::from_utc(datetime, Utc))
+    }
+}
+
+impl FromStr for Time {
+    type Err = chrono::ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match format!("{}T00:00:00Z", s).parse::<DateTime<Utc>>() {
+            Ok(dt) => Ok(dt.into()),
+            Err(e) => Err(e),
+        }
+    }
+}
